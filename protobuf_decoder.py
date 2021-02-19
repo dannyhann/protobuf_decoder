e1ac0b978b933190c6e2d8a73bf1e8ef012561d6
danny han <rhrnak0501@gmail.com>
2021-02-19 11:49:03 +0900

    Add protobuf decoder

---
+++ protobuf_decoder.py
@@ -0,0 +1,263 @@
+from __future__ import annotations
+import re
+from typing import List, Tuple, Union
+from enum import Enum
+import binascii
+from dataclasses import dataclass
+
+HEX_PATTERN = "^[\\0-9a-fA-F\\s]+$"
+
+
+@dataclass
+class ParsedResult:
+    field: int
+    wire_type: str
+    data: Union[str, int, List[ParsedResult]]
+
+    def to_dict(self):
+        if isinstance(self.data, list):
+            data = [sub_result.to_dict() for sub_result in self.data]
+        else:
+            data = self.data
+
+        return dict(
+            field=self.field,
+            wire_type=self.wire_type,
+            data=data
+        )
+
+
+class State(Enum):
+    FIND_FIELD = 1
+    PARSE_VARINT = 2
+    PARSE_LENGTH_DELIMITED = 3
+    GET_DELIMITED_DATA = 4
+    TERMINATED = 5
+
+
+class WireType(Enum):
+    VARINT = 0
+    LENGTH_DELIMITED = 2
+
+    BIT64 = 1  # not supported
+    BIT32 = 5  # not supported
+
+    START_GROUP = 3  # deprecated
+    END_GROUP = 4  # deprecated
+
+
+class Utils:
+    @classmethod
+    def validate(cls, string: str) -> Tuple[bool, str]:
+        regex_validator = re.compile(HEX_PATTERN)
+        validate_result = regex_validator.match(string)
+        if validate_result is None:
+            return False, string
+
+        hex_string = validate_result.string
+        hex_string = hex_string.replace(" ", "")
+
+        if len(hex_string) % 2 != 0:
+            return False, hex_string
+
+        return True, hex_string
+
+    @classmethod
+    def get_chunked_list(cls, string) -> List[str]:
+        while string:
+            yield string[:2]
+            string = string[2:]
+
+    @classmethod
+    def hex_string_to_binary(cls, string) -> str:
+        return bin(int(string, 16))[2:].zfill(4)
+
+    @classmethod
+    def hex_string_to_decimal(cls, string) -> int:
+        return int(string, 16)
+
+    @classmethod
+    def hex_string_to_utf8(cls, string) -> str:
+        string = string.replace(" ", "")
+        return binascii.unhexlify(string).decode("utf-8")
+
+
+class BytesBuffer:
+    def __init__(self):
+        self._buffer = []
+
+    def append(self, byte_string):
+        self._buffer.append(byte_string)
+
+    @property
+    def buffered_data(self):
+        return self._buffer
+
+    def flush(self):
+        self._buffer = []
+
+    def __iter__(self):
+        return iter(self._buffer)
+
+
+class Fetcher:
+    def __init__(self):
+        self.data_length = 0
+        self.fetch_index = 0
+
+    def set_data_length(self, data_length):
+        self.data_length = data_length
+
+    @staticmethod
+    def valid(data_length):
+        if not isinstance(data_length, int):
+            raise TypeError(f"a int object is required, not {repr(type(data_length))}")
+
+        if data_length < 0:
+            raise ValueError(f"data_length should be positive")
+
+    def fetch(self):
+        self.fetch_index += 1
+
+    @property
+    def has_next(self):
+        return self.fetch_index < self.data_length - 1
+
+    def seek(self, index=0):
+        self.fetch_index = index
+
+
+class Parser:
+    def __init__(self):
+        self.buffer = BytesBuffer()
+        self.fetcher = Fetcher()
+        self.target_field = None
+        self.parsed_data: List[ParsedResult] = []
+        self.state = State.FIND_FIELD
+
+    @staticmethod
+    def has_next(chunk_bytes) -> bool:
+        return bool(chunk_bytes & 0x80)
+
+    @staticmethod
+    def get_value(chunk_bytes) -> int:
+        return chunk_bytes & 0x7F
+
+    @staticmethod
+    def parse_wire_type(chunk_bytes) -> Tuple[int, int]:
+        wire_type = chunk_bytes & 0x7
+        field = chunk_bytes >> 3
+        return wire_type, field
+
+    def get_buffered_value(self) -> int:
+        bit_value = 0
+        for idx, byte_string in enumerate(self.buffer):
+            bit_value += byte_string << (7 * idx)
+        return bit_value
+
+    def handler_find_field(self, chunk):
+        value = self.get_value(chunk)
+        if self.has_next(chunk):
+            self.buffer.append(value)
+        else:
+            self.buffer.append(value)
+            bit_value = self.get_buffered_value()
+            wire_type, field = self.parse_wire_type(bit_value)
+            self.target_field = field
+
+            if wire_type == WireType.VARINT.value:
+                self.state = State.PARSE_VARINT
+            elif wire_type == WireType.LENGTH_DELIMITED.value:
+                self.state = State.PARSE_LENGTH_DELIMITED
+            elif wire_type == (WireType.BIT32.value, WireType.BIT64.value,
+                               WireType.END_GROUP.value, WireType.START_GROUP.value):
+                raise ValueError(f"Unsupported wire type {wire_type}")
+            else:
+                self.state = State.TERMINATED
+            self.buffer.flush()
+
+    def parse_varint_handler(self, chunk):
+        value = self.get_value(chunk)
+        if self.has_next(chunk):
+            self.buffer.append(value)
+        else:
+            self.buffer.append(value)
+            bit_value = self.get_buffered_value()
+            self.parsed_data.append(
+                ParsedResult(
+                    field=self.target_field,
+                    wire_type="varint",
+                    data=bit_value
+                )
+            )
+
+            self.state = State.FIND_FIELD
+            self.buffer.flush()
+
+    def parse_length_delimited_handler(self, chunk):
+        value = self.get_value(chunk)
+        if self.has_next(chunk):
+            self.buffer.append(value)
+        else:
+            self.buffer.append(value)
+            data_length = self.get_buffered_value()
+            self.fetcher.set_data_length(data_length)
+
+            self.state = State.GET_DELIMITED_DATA
+
+            self.buffer.flush()
+
+    def get_delimited_data_handler(self, chunk):
+        value = chunk
+        if self.fetcher.has_next:
+            self.fetcher.fetch()
+            self.buffer.append(value)
+
+        else:
+            self.buffer.append(value)
+            data = list(map(lambda x: hex(x)[2:].zfill(2), self.buffer))
+            sub_parsed_date = Parser().parse(" ".join(data))
+            if sub_parsed_date:
+                data = sub_parsed_date
+                wire_type = "length_delimited"
+            else:
+                data = Utils.hex_string_to_utf8("".join(data))
+                wire_type = "string"
+
+            self.parsed_data.append(
+                ParsedResult(
+                    field=self.target_field,
+                    wire_type=wire_type,
+                    data=data
+                )
+            )
+            self.buffer.flush()
+            self.fetcher.seek()
+            self.state = State.FIND_FIELD
+
+    def parse(self, test_target):
+        is_valid, validate_string = Utils.validate(test_target)
+        if not is_valid:
+            raise ValueError("Invalid hex format")
+
+        for chunk in Utils.get_chunked_list(validate_string):
+            chunk = Utils.hex_string_to_decimal(chunk)
+
+            if self.state == State.FIND_FIELD:
+                self.handler_find_field(chunk)
+
+            elif self.state == State.PARSE_VARINT:
+                self.parse_varint_handler(chunk)
+
+            elif self.state == State.PARSE_LENGTH_DELIMITED:
+                self.parse_length_delimited_handler(chunk)
+
+            elif self.state == State.GET_DELIMITED_DATA:
+                self.get_delimited_data_handler(chunk)
+
+            elif self.state == State.TERMINATED:
+                return self.parsed_data
+            else:
+                raise ValueError(f"Unsupported State {self.state}")
+
+        return self.parsed_data
---
+++ tests.py
@@ -0,0 +1,223 @@
+import pytest
+import logging
+from protobuf_decoder import Utils, Parser, ParsedResult
+
+
+def test_binary_validate():
+    is_valid, _ = Utils.validate("08 12")
+    assert is_valid is True
+
+    is_valid, _ = Utils.validate("0812")
+    assert is_valid is True
+
+    is_valid, _ = Utils.validate("08 12 2")
+    assert is_valid is False
+
+    is_valid, _ = Utils.validate("081H")
+    assert is_valid is False
+
+
+def test_get_chunked_list():
+    _, validate_string = Utils.validate("08 12")
+    chunked_list = Utils.get_chunked_list(validate_string)
+    chunked_list = list(chunked_list)
+    assert len(chunked_list) == 2
+    assert chunked_list[0] == "08"
+    assert chunked_list[1] == "12"
+
+
+def test_hex_string_to_binary():
+    binary = Utils.hex_string_to_binary("1")
+    assert binary == "0001"
+
+    binary = Utils.hex_string_to_binary("F")
+    assert binary == "1111"
+
+    with pytest.raises(ValueError):
+        Utils.hex_string_to_binary("H")
+
+
+def test_hex_string_to_decimal():
+    assert Utils.hex_string_to_decimal("A") == 10
+    assert Utils.hex_string_to_decimal("1") == 1
+    assert Utils.hex_string_to_decimal("F") == 15
+    with pytest.raises(ValueError):
+        Utils.hex_string_to_decimal("G")
+
+
+def test_hex_string_to_utf8():
+    assert Utils.hex_string_to_utf8("74657374") == "test"
+    assert Utils.hex_string_to_utf8("74 65 73 74 32") == "test2"
+    assert Utils.hex_string_to_utf8("E2 9C 8A") == "✊"
+
+
+def test_parse():
+    """
+    # proto
+    message Test1 {
+      required int32 a = 16;
+    }
+
+    # message
+    {
+      "a": 1
+    }
+
+    # binary
+    80 01 01
+    """
+
+    test_target = "80 01 01"
+    parsed_data = Parser().parse(test_target)
+    assert parsed_data == [ParsedResult(field=16, wire_type="varint", data=1)]
+    assert ParsedResult(field=16, wire_type="varint", data=1).to_dict() == dict(field=16, wire_type="varint", data=1)
+
+
+def test_parse2():
+    """
+    # proto
+    message Test1 {
+      int32 a = 1;
+      string b = 2;
+    }
+
+    # message
+    {
+      "a": 150,
+      "b": "test"
+    }
+
+    # binary
+    08 96 01 12 04 74 65 73 74
+    """
+
+    test_target = "08 96 01 12 04 74 65 73 74"
+    parsed_data = Parser().parse(test_target)
+    assert parsed_data == [ParsedResult(field=1, wire_type="varint", data=150),
+                           ParsedResult(field=2, wire_type="string", data='test')]
+
+
+def test_parse3():
+    """
+    # proto
+    message Test1 {
+      string b = 2;
+    }
+
+    # message
+    {
+      "b": "testing"
+    }
+
+    # binary
+    12 07 74 65 73 74 69 6e 67
+    """
+    test_target = "12 07 74 65 73 74 69 6e 67"
+    parsed_data = Parser().parse(test_target)
+    assert parsed_data == [ParsedResult(field=2, wire_type="string", data='testing')]
+
+
+def test_parser4():
+    """
+    # proto
+    message Test1 {
+      int32 a = 1;
+    }
+
+    message Test2 {
+      Test1 b = 3;
+    }
+
+    # message
+    {
+      "a": {
+            "b": 150
+            }
+    }
+
+    # binary
+    1a 03 08 96 01
+    """
+    test_target = "1a 03 08 96 01"
+    parsed_data = Parser().parse(test_target)
+    assert parsed_data == [
+        ParsedResult(field=3, wire_type="length_delimited", data=[
+            ParsedResult(field=1, wire_type="varint", data=150)
+        ])]
+
+
+def test_parser5():
+    """
+    # proto
+    message Test1 {
+      string a = 1;
+    }
+
+    # message
+    {
+      "a": "테스트"
+    }
+
+    # binary
+    0A 09 ED 85 8C EC 8A A4 ED 8A B8
+    """
+    test_target = "0A 09 ED 85 8C EC 8A A4 ED 8A B8"
+    parsed_data = Parser().parse(test_target)
+    assert parsed_data == [ParsedResult(field=1, wire_type="string", data='테스트')]
+
+
+def test_parser6():
+    """
+    ['ed', '85', '8c', 'ec', '8a', 'a4', 'ed', '8a', 'b8'] is "테스트"
+    """
+    test_target = " ".join(['ed', '85', '8c', 'ec', '8a', 'a4', 'ed', '8a', 'b8'])
+    parsed_data = Parser().parse(test_target)
+    assert bool(parsed_data) is False
+
+
+def test_parser7():
+    """
+    # proto
+    message Test1 {
+      required string a = 1;
+    }
+
+    # message
+    {
+      "a": "✊"
+    }
+
+    # binary
+    0A 03 E2 9C 8A
+
+    """
+    test_target = "0A 03 E2 9C 8A"
+    parsed_data = Parser().parse(test_target)
+    assert parsed_data == [ParsedResult(field=1, wire_type="string", data='✊')]
+
+
+def test_parser8():
+    """
+    # proto
+    message Test1 {
+      repeated string a = 1;
+    }
+
+    # message
+    {
+      "a": [
+        "test",
+        "test2"
+      ]
+    }
+
+    # binary
+    0A 04 74 65 73 74 0A 05 74 65 73 74 32
+
+    """
+    test_target = "0A 04 74 65 73 74 0A 05 74 65 73 74 32"
+    parsed_data = Parser().parse(test_target)
+    assert parsed_data == [
+        ParsedResult(field=1, wire_type="string", data='test'),
+        ParsedResult(field=1, wire_type="string", data='test2')
+    ]
