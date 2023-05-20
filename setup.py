6cdea3c4065a5b8f2fe8dd9d354ffbe415d5660a
danny han <rhrnak0501@gmail.com>
2023-05-20 20:09:17 +0900

    Release 0.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="protobuf_decoder",
-    version="0.1.0",
+    version="0.2.0",
     author="danny han",
     author_email="rhrnak0501@gmail.com",
     description="Decode protobuf without proto file",
